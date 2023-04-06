ace07a9feb7e31d77c7f617fd7f45ee335160e6b
Stefano <stedamb@gmail.com>
2023-04-06 15:43:52 +0200

    added geometries in hero sec

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,14 +9,14 @@ const Hero = () => {
             <div
                 className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
                 <div className='flex flex-col justify-center items-center mt-5'>
-                    <div className='w-8 h-8 rounded-full bg-[#915EFF] mt-5'/>
-                    <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+                    <div className='w-8 h-8 rounded-full bg-green-700 mt-3'/>
+                    <div className='w-1 sm:h-80 h-40 black-gradient'/>
                 </div>
 
                 <div>
                     <h1 className={`${styles.heroHeadText} text-white`}>
                         Hi, I'm
-                        <span className='text-[#915EFF] ml-3'>Stefano</span>
+                        <span className='text-green-600 ml-3'>Stefano</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
                         I develop 3D visuals, user
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,15 +1,25 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Computers = () => {
     const computer = useGLTF('./desktop_pc/scene.gltf');
+    const mesh = useRef();
+
+    useFrame(() => {
+        if (!mesh.current) {
+            return;
+        }
+
+        mesh.current.rotation.x += 0.01;
+        mesh.current.rotation.y += 0.001;
+    });
 
     return (
-        <mesh>
-            <hemisphereLight intensity={0.15} groundColor='black'/>
+        <mesh ref={mesh}>
+            <hemisphereLight intensity={0.25} groundColor='black'/>
             <spotLight
                 position={[-20, 50, 10]}
                 angle={0.12}
@@ -18,11 +28,17 @@ const Computers = () => {
                 castShadow
                 shadow-mapSize={1024}/>
             <pointLight intensity={1}/>
-            <primitive
+            <torusGeometry />
+            <meshStandardMaterial color='orange' wireframe />
+            {/* <primitive
                 object={computer.scene}
                 scale={0.75}
                 position={[0, -3.25, -1.5]}
-                rotation={[-0.01, -0.2, -0.1]}/>
+                rotation={[-0.01, -0.2, -0.1]}/> */}
+                {/* <mesh>
+                    <boxGeometry args={[2,2,2]}/>
+                    <meshStandardMaterial color='white' wireframe />
+                </mesh> */}
         </mesh>
     )
 }
@@ -30,11 +46,11 @@ const Computers = () => {
 const ComputersCanvas = () => {
     return (
         <Canvas
-            frameloop='deamnd'
+            // frameloop='demand'
             shadows
             camera={{
             position: [
-                20, 3, 5
+                10, 3, 5
             ],
             fov: 25
         }}
@@ -48,8 +64,10 @@ const ComputersCanvas = () => {
                     minPolarAngle={Math.PI / 2}/>
                 <Computers/>
             </Suspense>
+
+            <Preload all />
         </Canvas>
     );
 };
 
-export default Computers
+export default ComputersCanvas;
